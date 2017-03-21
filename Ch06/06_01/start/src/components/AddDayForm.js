0bf272bd3cf48288dac040cd0fb88e5f7b5f6ee9
Jake Ninness <jake.ninness@gmail.com>
2017-03-21 17:35:58 +1300

    adding a form

--- Ch06/06_01/start/src/components/AddDayForm.js
+++ Ch06/06_01/start/src/components/AddDayForm.js
@@ -1,8 +1,50 @@
-import { PropTypes } from 'react'
+import { PropTypes, Component } from 'react'
 
-export const AddDayForm = () => (
-	<h1>Add A Day</h1>
-)
+export const AddDayForm = ({ resort, date, powder, backcountry }) => {
+
+	let _resort, _date, _powder, _backcountry
+
+	const submit = (e) => {
+		e.preventDefault()
+		console.log('resort', _resort.value)
+		console.log('date', _date.value)
+		console.log('powder', _powder.checked)
+		console.log('backcountry', _backcountry.checked)
+	}
+
+
+
+		return (
+			<form onSubmit={submit} className="add-day-form" >
+
+				<label htmlFor="resort">Resort Name</label>
+				<input type="text" id="resort" required defaultValue={resort} ref={input => _resort = input} />
+
+				<label htmlFor="date">Date</label>
+				<input type="date" id="date"  required defaultValue={date} ref={input => _date = input} />
+
+				<div>
+					<input type="checkbox" id="powder" defaultChecked={powder} ref={input => _powder = input}/>
+					<label htmlFor="powder">Powder Day</label>
+				</div>
+
+				<div>
+					<input type="checkbox" id="backcountry" defaultChecked={backcountry} ref={input => _backcountry = input} />
+					<label htmlFor="backcountry">Backcountry Day</label>
+				</div>
+				<button>Add Day</button>
+			</form>
+		)
+
+}
+
+
+AddDayForm.defaultProps = {
+	resort: "Turoa",
+	date: "2017-02-12",
+	powder: true,
+	backcountry: false
+}
 
 AddDayForm.propTypes = {
 	resort: PropTypes.string.isRequired,
@@ -10,3 +52,4 @@ AddDayForm.propTypes = {
 	powder: PropTypes.bool.isRequired,
 	backcountry: PropTypes.bool.isRequired
 }
+
