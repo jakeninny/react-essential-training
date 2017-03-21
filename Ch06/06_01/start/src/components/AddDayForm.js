e95c315f6c9c597aecf97eb70885085cbcefc1b9
Jake Ninness <jake.ninness@gmail.com>
2017-03-21 17:51:50 +1300

    more form functionality data binding across app

--- Ch06/06_01/start/src/components/AddDayForm.js
+++ Ch06/06_01/start/src/components/AddDayForm.js
@@ -1,50 +1,110 @@
 import { PropTypes, Component } from 'react'
 
-export const AddDayForm = ({ resort, date, powder, backcountry }) => {
+const tahoeResorts = [
+	"Alpine Meadows",
+	"Boreal",
+	"Diamond Peak",
+	"Donner Ski Ranch", 
+	"Heavenly", 
+	"Homewood",
+	"Kirkwood",
+	"Mt. Rose", 
+	"Northstar",
+	"Squaw Valley",
+	"Sugar Bowl"
+]
 
-	let _resort, _date, _powder, _backcountry
-
-	const submit = (e) => {
-		e.preventDefault()
-		console.log('resort', _resort.value)
-		console.log('date', _date.value)
-		console.log('powder', _powder.checked)
-		console.log('backcountry', _backcountry.checked)
+class AutoComplete extends Component {
+	get value() {
+		return this.refs.inputResort.value
 	}
 
+	set value(inputValue) {
+		this.refs.inputResort.value = inputValue
+	}
 
-
+	render() {
 		return (
-			<form onSubmit={submit} className="add-day-form" >
-
-				<label htmlFor="resort">Resort Name</label>
-				<input type="text" id="resort" required defaultValue={resort} ref={input => _resort = input} />
-
-				<label htmlFor="date">Date</label>
-				<input type="date" id="date"  required defaultValue={date} ref={input => _date = input} />
-
-				<div>
-					<input type="checkbox" id="powder" defaultChecked={powder} ref={input => _powder = input}/>
-					<label htmlFor="powder">Powder Day</label>
-				</div>
-
-				<div>
-					<input type="checkbox" id="backcountry" defaultChecked={backcountry} ref={input => _backcountry = input} />
-					<label htmlFor="backcountry">Backcountry Day</label>
-				</div>
-				<button>Add Day</button>
-			</form>
+			<div>
+				<input type="text" list="tahoe-resorts" ref="inputResort" />
+				<datalist id="tahoe-resorts" >
+					{this.props.options.map(
+						(opt, i) => <option key={i} > {opt} </option>
+					)}
+				</datalist>
+			</div>
 		)
+	}
+} 
 
+export const AddDayForm = ({ resort, 
+							 date, 
+							 powder, 
+							 backcountry,
+							 onNewDay }) => {
+	
+	let _resort, _date, _powder, _backcountry
+	
+	const submit = (e) => {
+		e.preventDefault()
+		onNewDay({
+			resort: _resort.value,
+			date: _date.value,
+			powder: _powder.checked,
+			backcountry: _backcountry.checked
+		})
+		_resort.value = ''
+		_date.value = ''
+		_powder.checked = false
+		_backcountry.checked = false
+
+	}
+
+	return (
+		<form onSubmit={submit} className="add-day-form">
+
+			<label htmlFor="resort">Resort Name</label>
+			<AutoComplete options={tahoeResorts}
+				   ref={input => _resort = input}/>
+
+			<label htmlFor="date">Date</label>
+			<input id="date" 
+				   type="date" 
+				   required 
+				   defaultValue={date}
+				   ref={input => _date = input}/>
+
+			<div>
+				<input id="powder" 
+					   type="checkbox" 
+					   defaultChecked={powder}	
+					   ref="powder"
+					   ref={input => _powder = input}/>
+				<label htmlFor="powder">Powder Day</label>
+			</div>
+
+			<div>	
+				<input id="backcountry" 
+					   type="checkbox"
+					   defaultChecked={backcountry} 
+					   ref="backcountry"
+					   ref={input => _backcountry = input}/>
+				<label htmlFor="backcountry">
+					Backcountry Day
+				</label>
+			</div>
+			<button>Add Day</button>
+		</form>
+	)
 }
 
-
 AddDayForm.defaultProps = {
-	resort: "Turoa",
+	resort: "Kirkwood",
 	date: "2017-02-12",
 	powder: true,
 	backcountry: false
 }
+
 
 AddDayForm.propTypes = {
 	resort: PropTypes.string.isRequired,
@@ -52,4 +112,3 @@ AddDayForm.propTypes = {
 	powder: PropTypes.bool.isRequired,
 	backcountry: PropTypes.bool.isRequired
 }
-
